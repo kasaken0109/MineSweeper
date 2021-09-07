474593cf2f5299dcb1607c94dc335b08d5959128
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-07 22:40:06 +0900

    ビンゴのセルクラスを作成した

---
+++ Assets/Script/CellBase.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellBase : MonoBehaviour
+{
+    private int bingoNum;
+    private int score = 1;
+
+    public int Score
+    {
+        get => score;
+    }
+
+    [SerializeField] GameObject m_cover = null;
+
+    public int BingoNum
+    {
+        get => bingoNum;
+        set
+        {
+            bingoNum = value;
+        }
+    }
+
+    public bool IsOpen;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        IsOpen = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public enum CellDisplayState
+    {
+        Cover = 0,
+        Uncover = 1,
+        Checked = 2,
+    }
+
+    public void CheckOpen(int num)
+    {
+        if(num == bingoNum)
+        {
+            m_cover?.SetActive(true);
+            IsOpen = true;
+            score = 2;
+        }
+    } 
+
+    public void SetPanel(bool IsActive)
+    {
+        if(IsActive) m_cover?.SetActive(true);
+        else
+        {
+            m_cover?.SetActive(false);
+        }
+    }
+}
+
+
